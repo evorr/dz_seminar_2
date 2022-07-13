fabff998ba15216fbfbee20fbc78caf69b726be3
Elena <9513495670@mail.ru>
2022-07-13 17:45:12 +0300

    added zadacha 13

---
+++ zadacha_13/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа 
+//или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+double SecDigit(int num)
+{
+    double res = (num%1000)/100;
+    if(res>=0)return res;
+    else{
+        res=res*-1;}
+    return res;
+}
+int secondDigit=Convert.ToInt32(SecDigit(num));
+if (num<100&&num>=-100)Console.WriteLine($"{num} -> третьей цифры нет");
+else Console.WriteLine($"{num} -> {secondDigit}");
+
