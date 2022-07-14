64070e0e7d605e8b823e591df9c84980d62e7a4c
Elena <9513495670@mail.ru>
2022-07-14 09:16:47 +0300

    поправлена задача 15

--- zadacha_15/Program.cs
+++ zadacha_15/Program.cs
@@ -6,5 +6,15 @@
 
 Console.Write("Введите число от 1 до 7: ");
 int num = Convert.ToInt32(Console.ReadLine());
-if (num<6) Console.WriteLine($"{num} -> нет");
-else Console.WriteLine($"{num} -> да");
+int Check(int number)
+{
+    if (number>=1&&number<=7)return number;
+    else{
+        Console.Write("Введите число от 1 до 7: ");
+        int newnum = Convert.ToInt32(Console.ReadLine());
+        return Check(newnum);
+    }
+}
+int day = Check(num);
+if (day<6) Console.WriteLine($"{day} -> нет");
+else Console.WriteLine($"{day} -> да");
